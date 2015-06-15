12c8fdc2fd2434b98e18ff99c197e3c7b4ed9c97
Marco Rosas <marco.rosas@outlook.com>
2015-06-15 17:18:24 -0600

    login_controller

---
+++ public/index.php
@@ -0,0 +1,11 @@
+<?php
+
+if (!isset($action)) {
+  $action = 'login';
+}
+
+if ($action == 'login') {
+  include 'login.php';
+}
+
+ ?>
--- public/view/header.php
+++ public/view/header.php
@@ -4,12 +4,12 @@
 <!-- the head section -->
 <head>
     <title>The Inquisition Contest</title>
-    <link rel="stylesheet" type="text/css" href="../style/style.css">
+    <link rel="stylesheet" type="text/css" href="style/style.css">
     <meta charset="UTF-8">
 </head>
 
 <!-- the body section -->
 <body>
 <header>
-    <h1>The Inquisition</h1>
+    <h1 class="title">The Inquisition</h1>
 </header>
