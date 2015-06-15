ed274f83b318382c4e219882dec0507c86c2eaca
Marco Rosas <marco.rosas@outlook.com>
2015-06-15 16:58:52 -0600

    ignoreFiles

---
+++ public/aux/header.php
@@ -0,0 +1,15 @@
+<?php
+
+echo '<header class="mainColorTheme">
+  <a href="/index.php">
+    <div class="logoframe mainLogoFont">theRoomie</div>
+  </a>
+
+  <nav class="main">
+    <a href="/index.php">Home</a>
+    |
+    <a href="/indv-website/support.html">Support</a>
+  </nav>
+</header>';
+
+?>
